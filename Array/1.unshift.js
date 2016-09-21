39e4bf7e03658387591cc0cc14552a635ab9b314
unknown <>
2016-09-21 23:07:22 +0800

    sublime

---
+++ Array/1.unshift.js
@@ -0,0 +1,7 @@
+// 1. 开头增加一个或者多个元素
+// 2. 返回 newAry.length
+// 3. 元素组增加
+var ary1 = ['a', 'b', 'c', 'd'];
+var newAry1 = ary1.unshift('new');
+console.log(newAry1);
+console.log(ary1);
---
+++ a.js
@@ -0,0 +1,11 @@
+fdf
+function a () {
+
+}
+a = 123;
+ffdsa
+
+if ()
+
+var a = dafas;
+
